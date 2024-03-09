61fdd9fe85aa862ae09635a808216fff6584d255
Vladislav Romanov <vladislav.romanov@ansiblehealth.com>
2024-03-10 01:46:33 +0300

    refactor: updated server file

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const authRoutes = require('./routes/auth')
+const path = require("path");
 
 // express app
 const app = express()
@@ -17,6 +18,14 @@ app.use((req, res, next) => {
 
 // routes
 app.use('/api/auth', authRoutes)
+
+// Serve the static build files
+app.use(express.static(path.join(__dirname, "build")));
+
+// Handle all other routes and serve the index.html
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
